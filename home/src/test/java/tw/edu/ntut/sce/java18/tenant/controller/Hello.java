0905906066f5310ccb48793aa357e17ebc3d9dd9
Bo-Xuan Fan <coffee0127@gmail.com>
2022-08-07 15:32:24 +0800

    test: add missing Hello

---
+++ home/src/test/java/tw/edu/ntut/sce/java18/tenant/controller/Hello.java
@@ -0,0 +1,3 @@
+package tw.edu.ntut.sce.java18.tenant.controller;
+
+public class Hello {}
--- home/src/test/java/tw/edu/ntut/sce/java18/tenant/controller/HelloController.java
+++ home/src/test/java/tw/edu/ntut/sce/java18/tenant/controller/HelloController.java
@@ -1,7 +1,5 @@
 package tw.edu.ntut.sce.java18.tenant.controller;
 
-import tw.edu.ntut.sce.java18.common.Hello;
-
 public class HelloController {
 
   public static void main(String[] args) {
