92292da212f58a20f39b44d4c917d433af2a3425
Bo-Xuan Fan <coffee0127@gmail.com>
2022-08-07 15:26:26 +0800

    test: add HelloController

---
+++ home/src/test/java/tw/edu/ntut/sce/java18/tenant/controller/HelloController.java
@@ -0,0 +1,10 @@
+package tw.edu.ntut.sce.java18.tenant.controller;
+
+import tw.edu.ntut.sce.java18.common.Hello;
+
+public class HelloController {
+
+  public static void main(String[] args) {
+    System.out.println(new Hello());
+  }
+}
